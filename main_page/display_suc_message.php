66a1175f6a4f5c14f5ece555c00cf25cbc055d93
Rodrigo Ferreira <rodrigo.e09@hotmail.com>
2024-04-16 12:36:35 +0100

    pop up message fixed

--- main_page/display_suc_message.php
+++ main_page/display_suc_message.php
@@ -1,9 +1,16 @@
 
-<?php if(isset($_GET['meg']) && @is_null($_GET['meg'])) {
-        echo '<div  class="alert alert-success" role="alert">';
-        echo "Thank You! I will be in touch soon.";
-        echo '</div>';
-    } else { 
-        echo '';
+<?php
+    if(isset($_GET['mg'])){
+        $var=$_GET['mg'];
+        if($var == 't') {
+            echo '<div  class="alert alert-success" role="alert">';
+            echo "Thank You! I will be in touch soon.";
+            echo '</div>';
+        } if($var == 'f') { 
+            echo '<div  class="alert alert-danger" role="alert">';
+            echo "Something went wrong. Please try again later.";
+            echo '</div>';
+        }
     }
+    
 ?> 
--- send_email.php
+++ send_email.php
@@ -12,14 +12,15 @@ $msg = "Name: " . $_POST["name"] . "\nEmail: " . $_POST["email"] . "\nMessage:\n
 //$msg = wordwrap($msg,70);
 
 // send email
-mail($to,$subject,$msg,$headers);
-
-if ($error == false) {
-    $result='<div class="alert alert-success">Thank You! I will be in touch soon.</div>';
-}else {
-    $result='<div class="alert alert-danger">Sorry there was an error sending your message. Please try again later</div>';
+$mail = mail($to,$subject,$msg,$headers);
+if(!$mail) {   
+    $mg = "f"  
+} else {
+    $mg = "t"
 }
 
-header("Location: https://rodrigoferreira.dev?meg=t");
+$mg = "t"
+
+header("Location: https://rodrigoferreira.dev? mg = $mg");
 die();
 ?>
